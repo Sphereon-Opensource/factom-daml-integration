461da49a49ffed91e06e36846ee0f62d6ee624fe
LDMGN <lgijsen@sphereon.com>
2019-07-30 14:02:57 +0200

    SPHEREON-674 Added support for reading token configurations from JSON files

--- src/main/java/com/sphereon/da/ledger/mithra/utils/FatToken.java
+++ src/main/java/com/sphereon/da/ledger/mithra/utils/FatToken.java
@@ -1,9 +1,9 @@
 package com.sphereon.da.ledger.mithra.utils;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
 
 public class FatToken {
-
     private final String tokenId;
     private final String tokenChainId;
     private final String issuerRootChainId;
@@ -12,9 +12,11 @@ public class FatToken {
     //Store private key hardcoded for demo purposes (Factom testnet). Implement a keyvault for production
     private final String identityLevel1SecretAddress;
 
-    public FatToken(@JsonProperty("tokenId") String tokenId, @JsonProperty("tokenChainId") String tokenChainId,
-                    @JsonProperty("issuerRootChainId") String issuerRootChainId, @JsonProperty("coinbasePublicAddress") String coinbaseAddressPublic,
-                    @JsonProperty("identityLevel1SecretAddress") String identityLevel1SecretAddress){
+    public FatToken(@JsonProperty("tokenId") final String tokenId,
+                    @JsonProperty("tokenChainId") final String tokenChainId,
+                    @JsonProperty("issuerRootChainId") final String issuerRootChainId,
+                    @JsonProperty("coinbaseAddressPublic") final String coinbaseAddressPublic,
+                    @JsonProperty("identityLevel1SecretAddress") final String identityLevel1SecretAddress) {
         this.tokenId = tokenId;
         this.tokenChainId = tokenChainId;
         this.issuerRootChainId = issuerRootChainId;
@@ -22,12 +24,35 @@ public class FatToken {
         this.identityLevel1SecretAddress = identityLevel1SecretAddress;
     }
 
-    public String getTokenId(){return tokenId;}
-    public String getTokenChainId(){return tokenChainId;}
-    public String getIssuerRootChainId(){return issuerRootChainId;}
-    public String getIdentityLevel1SecretAddress(){return identityLevel1SecretAddress;}
+    public String getTokenId() {
+        return tokenId;
+    }
 
-    public String getCoinbaseAddressPublic() {return coinbaseAddressPublic;}
+    public String getTokenChainId() {
+        return tokenChainId;
+    }
+
+    public String getIssuerRootChainId() {
+        return issuerRootChainId;
+    }
+
+    public String getIdentityLevel1SecretAddress() {
+        return identityLevel1SecretAddress;
+    }
+
+    public String getCoinbaseAddressPublic() {
+        return coinbaseAddressPublic;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("tokenId", tokenId)
+                .add("tokenChainId", tokenChainId)
+                .add("issuerRootChainId", issuerRootChainId)
+                .add("coinbaseAddressPublic", coinbaseAddressPublic)
+                .toString();
+    }
 }
 
 
