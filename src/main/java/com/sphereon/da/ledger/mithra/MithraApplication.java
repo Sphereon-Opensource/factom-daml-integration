c5642626ae732cc8ac0759b05b564bbbbcf9c866
Scott Malley <srmalley-sphereon@github.com>
2019-07-30 13:11:21 +0200

    initial commit

---
+++ src/main/java/com/sphereon/da/ledger/mithra/MithraApplication.java
@@ -0,0 +1,13 @@
+package com.sphereon.da.ledger.mithra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MithraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MithraApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sphereon/da/ledger/mithra/utils/TokenUtils.java
@@ -0,0 +1,15 @@
+package com.sphereon.da.ledger.mithra.utils;
+
+import org.springframework.context.annotation.Bean;
+import java.util.Arrays;
+import java.util.List;
+
+public class TokenUtils {
+    @Bean
+    public List<FatToken> fatTokenList(){
+        return new Arrays.asList(new FatToken("tokenId1", "tokenChainId1",
+                "issuerRootChainId1","coinbaseAddress1","sk11"),
+                new FatToken("tokenId2", "tokenChainId2",
+                        "issuerRootChainId2","coinbaseAddress2","sk12"));
+    }
+}
